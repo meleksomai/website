108c5b812d24b540c6836ee7b927c092518355c2
Melek <1846037+meleksomai@users.noreply.github.com>
2022-08-14 11:41:07 -0500

    fix spotify (#14)

--- apps/web/components/Footer.tsx
+++ apps/web/components/Footer.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
-import { Text, Section, Flex } from "@thugga/ui";
+import { Text, Section, Box } from "@thugga/ui";
 
 import Spotify from "./Spotify";
 
@@ -9,23 +9,19 @@ import profilePic from "../public/images/spotify.svg";
 
 const DEFAULT_TAG = "div";
 
-export const Footer = React.forwardRef<
-  React.ElementRef<typeof DEFAULT_TAG>
-  //   NextLinkProps
->((props, forwardedRef) => {
+export const Footer = () => {
   return (
     <Section css={{ pt: "$8" }}>
-      <Flex>
-        <Flex css={{ width: "$5", mr: "$1" }}>
-          <Image src={profilePic} />
-        </Flex>
-        <Flex>
-          <Spotify />
-        </Flex>
-      </Flex>
-      <Text size="1">Made with love from Wisconsin, Illinois, and Tunis.</Text>
+      <Box css={{ py: "$2" }}>
+        <Spotify />
+      </Box>
+      <Box css={{ py: "$2" }}>
+        <Text size="1">
+          Made with love from Wisconsin, Illinois, and Tunis.
+        </Text>
+      </Box>
     </Section>
   );
-});
+};
 
 Footer.displayName = "Footer";
--- apps/web/components/Spotify.tsx
+++ apps/web/components/Spotify.tsx
@@ -1,41 +1,53 @@
+import Image from "next/image";
 import { useEffect } from "react";
 import useSWR from "swr";
 
-import { Text, Section } from "@thugga/ui";
+import { Text, Flex } from "@thugga/ui";
 
 import { NextLink } from "./NextLink";
 
 import fetcher from "../lib/fetcher";
+import spotifyLogo from "../public/images/spotify.svg";
 
-export default function Spotify() {
-  const { data } = useSWR<any>("/api/spotify", fetcher);
+const CurrentSong = (props: any) => {
+  const { data } = props;
 
-  if (data?.isPlaying) {
-    return (
-      <Section>
-        <NextLink href={data.songUrl} variant="simple">
-          <Text inline bold>
-            {data.title}
-          </Text>
-          <Text inline> - </Text>
-          <Text inline variant="light" bold>
-            {data.artist}
-          </Text>
+  return (
+    <Flex wrap="wrap">
+      <Text inline bold>
+        <NextLink href={data.songUrl ?? ""} variant="simple">
+          {data.title}
+          {data.artist}
         </NextLink>
         <Text inline variant="light">
           {" "}
           - Spotify{" "}
         </Text>
-      </Section>
-    );
-  } else {
-    return (
-      <Section>
-        <Text bold inline>
-          Not Playing
-        </Text>
+      </Text>
+    </Flex>
+  );
+};
+
+const NotPlaying = () => {
+  return (
+    <Flex wrap="wrap">
+      <Text bold>
+        Not Playing
         <Text inline> - Spotify </Text>
-      </Section>
-    );
-  }
+      </Text>
+    </Flex>
+  );
+};
+
+export default function Spotify() {
+  const { data } = useSWR<any>("/api/spotify", fetcher);
+
+  return (
+    <Flex>
+      <Flex css={{ width: "$5", mr: "$1", minWidth: "$5" }}>
+        <Image src={spotifyLogo} alt="spotify logo" />
+      </Flex>
+      {data?.isPlaying ? <CurrentSong data={data} /> : <NotPlaying />}
+    </Flex>
+  );
 }
