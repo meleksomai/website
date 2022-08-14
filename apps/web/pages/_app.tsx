b292876275f803232f2f9e6e3dd638027aec9aba
Melek <1846037+meleksomai@users.noreply.github.com>
2022-08-13 19:07:13 -0500

    Adding Google Analytics (#12)
    * Adding Google Analytics
    
    * Update apps/web/pages/_app.tsx

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "next-themes";
-import { AppProps } from "next/app";
+import { AppProps, NextWebVitalsMetric } from "next/app";
 import Head from "next/head";
+import { GoogleAnalytics, usePageViews, event } from "nextjs-google-analytics";
 import React from "react";
 
 import { Layout, darkTheme } from "@thugga/ui";
@@ -11,7 +12,25 @@ import globalStyles from "../styles/globalStyles";
 // https://github.com/modulz/stitches/issues/995
 globalStyles();
 
+export function reportWebVitals({
+  id,
+  name,
+  label,
+  value,
+}: NextWebVitalsMetric) {
+  event(name, {
+    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
+
 function App({ Component, pageProps }: AppProps) {
+  // Google Analytics
+  // - page views
+  usePageViews();
+
   return (
     <ThemeProvider
       // disableTransitionOnChange
@@ -19,6 +38,7 @@ function App({ Component, pageProps }: AppProps) {
       value={{ light: "light-theme", dark: darkTheme.className }}
       defaultTheme="system"
     >
+      <GoogleAnalytics />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
