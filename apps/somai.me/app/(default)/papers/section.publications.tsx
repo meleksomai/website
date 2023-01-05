fa1d2590d13c3f54d64d3eb02840ff56049f4ef1
Melek Somai <somaimelek@gmail.com>
2023-01-05 22:09:40 +0100

    added JMI

--- apps/somai.me/app/(default)/papers/section.publications.tsx
+++ apps/somai.me/app/(default)/papers/section.publications.tsx
@@ -14,35 +14,31 @@ export default async function PublicationsSection() {
       .sort((a, b) => b - a)
   );
   return (
-    <Section size="1">
-      <Box>
-        <Grid gap="5" columns="1">
-          {Array.from(years.values()).map((year) => (
-            <>
-              <Heading as="h3" size="2" css={{ pb: "$5" }}>
-                {year}
-              </Heading>
-              {publications
-                .filter(
-                  (a) => new Date(a.publishedAt.iso).getFullYear() === year
-                )
-                .map((paper) => (
-                  <Box key={paper.slug}>
-                    {/* <Separator size="2" /> */}
-                    <Heading as="h4" css={{ paddingBottom: "$2" }}>
-                      <NextLink href={`/paper/${paper.slug}`} variant="simple">
-                        {paper.citation.title}
-                      </NextLink>
-                    </Heading>
-                    <Text size="2" variant="light">
-                      Published by {paper.publisher} on {paper.publishedAt.text}
-                    </Text>
-                  </Box>
-                ))}
-            </>
-          ))}
-        </Grid>
-      </Box>
-    </Section>
+    <Box>
+      {Array.from(years.values()).map((year) => (
+        <Section size="1">
+          <Heading as="h3" size="2" css={{ pb: "$5" }}>
+            {year}
+          </Heading>
+          <Grid gap="5" columns="1">
+            {publications
+              .filter((a) => new Date(a.publishedAt.iso).getFullYear() === year)
+              .map((paper) => (
+                <Box key={paper.slug}>
+                  {/* <Separator size="2" /> */}
+                  <Heading as="h4" css={{ paddingBottom: "$2" }}>
+                    <NextLink href={`/paper/${paper.slug}`} variant="simple">
+                      {paper.citation.title}
+                    </NextLink>
+                  </Heading>
+                  <Text size="2" variant="light">
+                    Published by {paper.publisher} on {paper.publishedAt.text}
+                  </Text>
+                </Box>
+              ))}
+          </Grid>
+        </Section>
+      ))}
+    </Box>
   );
 }
