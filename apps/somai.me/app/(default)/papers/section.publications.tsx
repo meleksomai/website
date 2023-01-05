cf416b360c1e48366021fbc456b8709d074a9116
Melek Somai <somaimelek@gmail.com>
2023-01-05 21:54:58 +0100

    fixing error

--- apps/somai.me/app/(default)/papers/section.publications.tsx
+++ apps/somai.me/app/(default)/papers/section.publications.tsx
@@ -7,26 +7,39 @@ import { getAllPapers } from "../../../lib/papers";
 
 export default async function PublicationsSection() {
   const publications = await getAllPapers();
+  // Get years descending order
+  const years = new Set(
+    publications
+      .map((a) => new Date(a.publishedAt.iso).getFullYear())
+      .sort((a, b) => b - a)
+  );
   return (
     <Section size="1">
       <Box>
-        <Heading as="h3" size="3" css={{ py: "$3" }}>
-          {" "}
-          Academic Publications{" "}
-        </Heading>
         <Grid gap="5" columns="1">
-          {publications.map((paper) => (
-            <Box key={paper.slug}>
-              {/* <Separator size="2" /> */}
-              <Heading as="h4" size="2" css={{ paddingBottom: "$2" }}>
-                <NextLink href={`/paper/${paper.slug}`} variant="simple">
-                  {paper.citation.title}
-                </NextLink>
+          {Array.from(years.values()).map((year) => (
+            <>
+              <Heading as="h3" size="2" css={{ pb: "$5" }}>
+                {year}
               </Heading>
-              <Text size="2" variant="light">
-                Published by {paper.publisher} on {paper.publishedAt.text}
-              </Text>
-            </Box>
+              {publications
+                .filter(
+                  (a) => new Date(a.publishedAt.iso).getFullYear() === year
+                )
+                .map((paper) => (
+                  <Box key={paper.slug}>
+                    {/* <Separator size="2" /> */}
+                    <Heading as="h4" css={{ paddingBottom: "$2" }}>
+                      <NextLink href={`/paper/${paper.slug}`} variant="simple">
+                        {paper.citation.title}
+                      </NextLink>
+                    </Heading>
+                    <Text size="2" variant="light">
+                      Published by {paper.publisher} on {paper.publishedAt.text}
+                    </Text>
+                  </Box>
+                ))}
+            </>
           ))}
         </Grid>
       </Box>
