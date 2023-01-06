aa7e5fadf0b588ef7650d4b66930e54e455e8300
Melek Somai <somaimelek@gmail.com>
2023-01-06 01:33:54 +0100

    adding animation

--- apps/rethinkhealth.io/app/section.hero.tsx
+++ apps/rethinkhealth.io/app/section.hero.tsx
@@ -1,12 +1,15 @@
+"use client";
+
 import { Heading, Section, Box, Grid } from "@thugga/ui";
+import Spline from "@splinetool/react-spline";
 
 import { NextLink } from "../components";
 
 export default function HeroSection() {
   return (
     <Section size="2">
-      <Grid gap="5" columns={{ "@initial": "2", "@bp2": "3" }}>
-        <Box css={{ gridColumn: "span 2" }}>
+      <Grid gap="5" columns={{ "@initial": "1", "@bp2": "2" }}>
+        <Box css={{ gridColumn: "span 1" }}>
           <Heading as="p" size="4" css={{ pb: "$5" }} mono>
             We help healthcare systems through their{"  "}
             <Heading inline as="span" size="4" css={{ color: "$blue11" }}>
@@ -14,21 +17,33 @@ export default function HeroSection() {
             </Heading>
             journey.
           </Heading>
+          <Box css={{ gridColumn: "span 2" }}>
+            <Heading as="p" size="1" css={{ pb: "$5" }}>
+              Contact us today to learn more and see how we can help your
+              organization achieve its digital transformation goals at scale.
+            </Heading>
+            <NextLink
+              href="mailto:contact@rethinkhealth.io"
+              variant="blue"
+              size="large"
+            >
+              Contact us
+            </NextLink>
+          </Box>
         </Box>
-      </Grid>
-      <Grid gap="5" columns={{ "@initial": "2", "@bp2": "4" }}>
-        <Box css={{ gridColumn: "span 2" }}>
-          <Heading as="p" size="1" css={{ pb: "$5" }}>
-            Contact us today to learn more and see how we can help your
-            organization achieve its digital transformation goals at scale.
-          </Heading>
-          <NextLink
-            href="mailto:contact@rethinkhealth.io"
-            variant="blue"
-            size="large"
-          >
-            Contact us
-          </NextLink>
+        <Box
+          css={{
+            when: {
+              "@initial": {
+                display: "none",
+              },
+              "@bp2": {
+                display: "flex",
+              },
+            },
+          }}
+        >
+          <Spline scene="https://prod.spline.design/7it3ORQcGVeBwdhF/scene.splinecode" />
         </Box>
       </Grid>
     </Section>
