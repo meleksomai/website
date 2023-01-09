4a98e438264552e024c231b14f005aeb1d4eab23
Melek <1846037+meleksomai@users.noreply.github.com>
2023-01-09 13:42:51 +0100

    Adding wedding website (#28)
    * adding wedding
    
    * fixing the issue with the favicon

--- apps/wedding/app/head.tsx
+++ apps/wedding/app/head.tsx
@@ -6,8 +6,9 @@ import { NEXT_SEO_DEFAULT } from '../next-seo.config';
 export default function Head() {
   return (
     <>
-      <title>Melek Somai | Personal Website</title>
+      <title>Melek Somai. Imen Ben Abid. Wedding</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
       <NextSeo {...NEXT_SEO_DEFAULT} useAppDir={true} />
     </>
   );
