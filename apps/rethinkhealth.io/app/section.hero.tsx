dee5f8da5cd1fe2613ad81fb1e297fdf21fd60d2
Melek Somai <somaimelek@gmail.com>
2023-01-14 21:08:36 +0100

    adding h1 and h2

--- apps/rethinkhealth.io/app/section.hero.tsx
+++ apps/rethinkhealth.io/app/section.hero.tsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
     <Section size="2">
       <Grid gap="5" columns={{ "@initial": "2", "@bp2": "3" }}>
         <Box css={{ gridColumn: "span 2" }}>
-          <Heading as="p" size="4" css={{ pb: "$5" }} mono>
+          <Heading as="h1" size="4" css={{ pb: "$5" }} mono>
             We help healthcare systems through their{"  "}
             <Heading inline as="span" size="4" css={{ color: "$blue11" }}>
               digital transformation{"  "}
@@ -18,7 +18,7 @@ export default function HeroSection() {
       </Grid>
       <Grid gap="5" columns={{ "@initial": "2", "@bp2": "4" }}>
         <Box css={{ gridColumn: "span 2" }}>
-          <Heading as="p" size="1" css={{ pb: "$5" }}>
+          <Heading as="h2" size="1" css={{ pb: "$5" }}>
             Contact us today to learn more and see how we can help your
             organization achieve its digital transformation goals at scale.
           </Heading>
