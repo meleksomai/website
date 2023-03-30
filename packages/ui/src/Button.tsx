c50f69b537fe04c786d195e9aae7b2ba401b748a
Melek Somai <somaimelek@gmail.com>
2023-03-30 11:02:21 +0100

    improving button

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -9,10 +9,8 @@ export const Button = styled("button", {
   justifyContent: "center",
   borderRadius: 4,
   padding: "0 15px",
-  fontSize: 15,
   lineHeight: 1,
   fontWeight: 500,
-  height: 35,
   "&:hover": { cursor: "pointer" },
 
   variants: {
@@ -27,41 +25,56 @@ export const Button = styled("button", {
         fontFamily: "$mono",
       },
     },
+    size: {
+      small: {
+        fontSize: "$2",
+        height: 27,
+      },
+      medium: {
+        fontSize: "$3",
+        height: 35,
+      },
+      large: {
+        fontSize: "$5",
+        height: 55,
+      },
+    },
     variant: {
       simple: {
-        backgroundColor: "$mauve2",
-        color: "$mauve12",
+        backgroundColor: "$colors$mauve2",
+        color: "$colors$mauve12",
         boxShadow: "0 2px 10px $blackA7",
         "&:hover": { backgroundColor: "$colors$mauve5" },
         "&:focus": { boxShadow: `0 0 0 2px $colors$mauve10` },
       },
       dark: {
-        backgroundColor: "$slate12",
-        color: "$loContrast",
+        backgroundColor: "$colors$slate12",
+        color: "$colors$loContrast",
         boxShadow: "0 2px 10px $blackA7",
         "&:hover": { backgroundColor: "$slate11" },
         "&:focus": {
           boxShadow: `0 0 0 2px $colors$slate9`,
           backgroundColor: "$slate8",
+          color: "$hiContrast",
         },
       },
       orange: {
-        backgroundColor: "$orange3",
-        color: "$orange10",
+        backgroundColor: "$colors$orange3",
+        color: "$colors$orange10",
         boxShadow: "0 2px 10px $blackA7",
         "&:hover": { backgroundColor: "$colors$orange5" },
         "&:focus": { boxShadow: `0 0 0 2px $colors$orange10` },
       },
       violet: {
-        backgroundColor: "$violet10",
-        color: "$violet11",
+        backgroundColor: "$colors$violet10",
+        color: "$colors$violet11",
         boxShadow: "0 2px 10px $blackA7",
         "&:hover": { backgroundColor: "$colors$mauve3" },
         "&:focus": { boxShadow: `0 0 0 2px black` },
       },
       blue: {
-        backgroundColor: "$blue11",
-        color: "$loContrast",
+        backgroundColor: "$colors$blueA10",
+        color: "$colors$loContrast",
         boxShadow: "0 2px 10px $blackA7",
         "&:hover": {
           backgroundColor: "$colors$blue9",
@@ -72,8 +85,8 @@ export const Button = styled("button", {
         },
       },
       green: {
-        backgroundColor: "$green4",
-        color: "$green11",
+        backgroundColor: "$colors$green4",
+        color: "$colors$green11",
         "&:hover": { backgroundColor: "$green5" },
         "&:focus": { boxShadow: "0 0 0 2px $green7" },
       },
@@ -82,5 +95,6 @@ export const Button = styled("button", {
 
   defaultVariants: {
     variant: "simple",
+    size: "medium",
   },
 });
