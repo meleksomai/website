8db73bb072d432f0f778b16e4d94219d0f7354e2
Melek <somaimelek@gmail.com>
2023-08-02 22:56:22 -0500

    fixed markdoc

--- packages/markdoc/src/nodes/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as code } from "./code.markdoc";
-export { default as fence } from "./fence.markdoc";
-export { default as heading } from "./heading.markdoc";
-export { default as link } from "./link.markdoc";
-export { default as list } from "./list.markdoc";
-export { default as blockquote } from "./blockquote.markdoc";
