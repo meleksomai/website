2ce53cda5642c0c8d37acaccf3aa8daedeb158df
Melek <somaimelek@gmail.com>
2023-08-02 20:01:32 -0500

    removig website

--- apps/wedding/next.config.js
+++
@@ -1,9 +0,0 @@
-// const withTM = require("next-transpile-modules")(["@thugga/ui", "@thugga/markdoc"]);
-
-module.exports = {
-  experimental: {
-    appDir: true,
-  },
-  transpilePackages: ["@thugga/ui", "@thugga/markdoc"],
-  reactStrictMode: true,
-};
