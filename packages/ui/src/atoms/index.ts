972e75875bbdd5ddbeb4eb8a6592d36a8d89239a
Melek Somai <melek@rethinkhealth.io>
2025-12-05 20:22:46 -0500

    feat: migrate UI

--- packages/ui/src/atoms/index.ts
+++
@@ -1,5 +0,0 @@
-export { atoms } from "./atoms";
-export type { Atoms } from "./atoms";
-
-export { sprinkles } from "./sprinkles.css";
-export type { Sprinkles } from "./sprinkles.css";
--- packages/ui/src/tokens/breakpoint.ts
+++
@@ -1,9 +0,0 @@
-export const breakpoints = {
-  mobile: "0px",
-  tablet: "768px",
-  desktop: "1280px",
-} as const;
-
-export type Breakpoint = keyof typeof breakpoints;
-
-export const breakpointNames = Object.keys(breakpoints) as Breakpoint[];
