a874ed50ef8366876550c15ea50c9fae5a86399d
Melek Somai <melek@rethinkhealth.io>
2025-12-26 12:01:56 -0500

    adding agents

--- os/email/src/agent.tsx
+++ os/email/src/agent.tsx
@@ -1,3 +1,5 @@
+import { render } from "@workspace/transactional";
+import WelcomeEmail from "@workspace/transactional/emails/generic";
 import { Agent, type AgentEmail } from "agents";
 
 export type State = {
@@ -64,6 +66,13 @@ export class HelloEmailAgent extends Agent<Env, State> {
       console.log("Auto-response already sent. Skipping.");
       return;
     }
+
+    const emailContent = render(
+      <WelcomeEmail
+        content={`Hey, \n\n Thank you for reaching out. I will get back to you as soon as I can.\n\nIn the meantime, feel free to explore my [website](https://somai.me) to learn more about my work and projects.\n\nLooking forward to connecting with you!\n`}
+        footer="This message has been auto-generated."
+      />
+    );
 
     await this.replyToEmail(email, {
       subject: "Re: " + (email.headers.get("Subject") || "(No Subject)"),
