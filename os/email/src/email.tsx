8f0325bab7d6dabb50e78a192e80124518e4d854
Melek Somai <melek@rethinkhealth.io>
2025-12-26 12:52:42 -0500

    fixed

--- os/email/src/email.tsx
+++ os/email/src/email.tsx
@@ -5,7 +5,7 @@ export const emailContent = async () =>
   await render(
     <WelcomeEmail
       content={`Hey, \n\n Thank you for reaching out. I will get back to you as soon as I can.\n\nIn the meantime, feel free to explore my [website](https://somai.me) to learn more about my work and projects.\n\nLooking forward to connecting with you!\n`}
-      footer="This message has been auto-generated."
+      footer="This message was auto-generated."
       previewMessage="Thank you for reaching out!"
     />
   );
